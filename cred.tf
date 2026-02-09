6e8710a70677331d67bf763e711747c1035e800d
shashankv-s1 <shashank.v@sentinelone.com>
2026-02-09 18:16:37 +0530

    Create cred.tf

---
+++ cred.tf
@@ -0,0 +1,15 @@
+ "MyDB": {
+      "Properties": {
+        "AllocatedStorage": "DBAllocatedStorage",
+        "MasterUserPassword": "DBPassword",
+        "MonitoringInterval": "60",
+        "DBInstanceIdentifier": "DBInstanceID",
+        "DBName": "DBName",
+        "DBInstanceClass": "DBInstanceClass",
+        "Engine": "MySQL",
+        "EngineVersion": "8.0.16",
+        "MasterUsername": "DBUsername",
+        "MonitoringRoleArn": "arn:aws:iam::123456789012:role/rds-monitoring-role"
+      },
+      "Type": "AWS::RDS::DBInstance"
+    }
